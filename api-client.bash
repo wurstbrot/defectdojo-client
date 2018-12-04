a9d09e0503353eeacaa36e84ab88ec0c83e06476
Timo Pagel <github@timo-pagel.de>
2018-12-04 16:54:20 +0100

    Fix

--- api-client.bash
+++ api-client.bash
@@ -2,6 +2,7 @@
 # Author: Timo Pagel, 
 name=$1
 dt=$(date '+%Y-%m-%d %H:%M:%S')
+d=$(date '+%Y-%m-%d')
 DEFECT_DOJO_HOST_NAME="defectdojo"
 API_URI="/api/v2"
 DEFECTDOJO_HOST_URL="http://$DEFECT_DOJO_HOST_NAME:8000$API_URI"
@@ -11,8 +12,8 @@ function filldata {
 	ENGAGEMENT_ID=$2
 	VERIFIED=$3
 	LOCAL_FILE_PATH=$4
-	echo "importing for engagement $ENGAGEMENT_ID dependency-check-report.xml"
-	curl \
+	echo "importing for engagement $ENGAGEMENT_ID $LOCAL_FILE_PATH"
+	RESP=$(curl \
         	--silent \
 	        --header 'Accept: application/json' \
 	        --header 'X-CSRFToken: txUiZ9ic2u5YnDIsmYv4hDDVePysE9Cv2yHHnZ6EGxmBWpwATQOe8iTZ5oKVN2Oz' \
@@ -20,27 +21,27 @@ function filldata {
 	        -H 'Content-Type: multipart/form-data; boundary=3f3faabb083d4d24bf696e1d859f2c87' \
 	        --form "description=Test ($dt)" \
 	        --form 'minimum_severity=Info' \
-	        --form "engagement=$ENGAGEMENT_ID" \
+	        --form "engagement=$API_URI/engagements/$ENGAGEMENT_ID/" \
 	        --form 'skip_duplicates=false' \
 	        --form "verified=$VERIFIED" \
+	        --form "scan_date=$d" \
+	        --form "lead=/api/v2/users/1/" \
 	        --form 'close_old_findings=True' \
-	        --form "file=@$LOCAL_FILE_PATH" \
+	        --form "file=@/media/encrypted/git/defectdojo_api/examples/dependency/dependency-check-report-minus-AppleJavaExtensions-minus-spring-txt.xml" \
 	        --form "scan_type=Dependency Check Scan" \
-	        "$DEFECTDOJO_HOST_URL/import-scan/" > /dev/null
-}
+		"$DEFECTDOJO_HOST_URL/import-scan/" )
 
-function fillata1 {
-	filldata $1 $2 $3 "/media/encrypted/git/defectdojo_api/examples/dependency/dependency-check-report.xml"
-
+	RESP=$(curl -X GET --silent \
+		--header 'Accept: application/json' \
+                --header 'X-CSRFToken: txUiZ9ic2u5YnDIsmYv4hDDVePysE9Cv2yHHnZ6EGxmBWpwATQOe8iTZ5oKVN2Oz' \
+                --header "Authorization: Token $API_KEY" \
+ "$DEFECTDOJO_HOST_URL/findings/?false_p=false&severity=Medium&duplicate=false&test__engagement=$ENGAGEMENT_ID")
+	COUNT=$(echo $RESP | grep count  | sed 's#{"count":##' | sed 's#,.*##')
+	if [ "$COUNT" != "0" ]; then
+		echo "$COUNT unhandled vulnerabilties detected"
+		exit;
+	fi
 }
-function fillata2 {
-	filldata $1 $2 $3 "/media/encrypted/git/defectdojo_api/examples/dependency/dependency-check-report-minus-AppleJavaExtensions.xml"
-}
-
-function fillata3 {
-	filldata $1 $2 $3 "/media/encrypted/git/defectdojo_api/examples/dependency/dependency-check-report-minus-AppleJavaExtensions-minus-spring-txt.xml"
-}
-
 function is_int() { 
 	printf "%f" $1 >/dev/null 2>&1
 }
@@ -60,7 +61,7 @@ if ! is_int $PRODUCT_ID; then
 fi
 echo "created product with id $PRODUCT_ID"
 PRODUCT_HYPERLINK="$API_URI/products/$PRODUCT_ID/"
-RESP=$(curl --silent --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'X-CSRFToken: rQ1QIKFTf5Ub17v1Fsghy4Z3JjeXMoaxwZ3liAud0qSE4yNHmRxFXlIkmEoM4ZrH' --header "Authorization: Token $API_KEY" -X POST -d '{    "branch_tag": "develop", "version": "v1.0", "engagement_type": "CI/CD", "product": "'$PRODUCT_HYPERLINK'", "name": "CI/CD dep check develop", "target_start": "2018-06-01", "target_end": "2018-06-01", "active": "True", "pen_test": "False", "check_list": "False", "threat_model": "False", "status": "In Progress", "deduplication_on_engagement": "True"}' "$DEFECTDOJO_HOST_URL/engagements/")
+RESP=$(curl --silent --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'X-CSRFToken: rQ1QIKFTf5Ub17v1Fsghy4Z3JjeXMoaxwZ3liAud0qSE4yNHmRxFXlIkmEoM4ZrH' --header "Authorization: Token $API_KEY" -X POST -d '{    "branch_tag": "develop", "version": "v1.0", "engagement_type": "CI/CD", "product": "'$PRODUCT_HYPERLINK'", "name": "CI/CD dep check develop", "target_start": "2018-06-01", "target_end": "2018-06-01", "active": "True", "pen_test": "False", "check_list": "False", "threat_model": "False", "status": "In Progress", "deduplication_on_engagement": "True", "lead": "/api/v2/users/1/"}' "$DEFECTDOJO_HOST_URL/engagements/")
 
 ENGAGEMENT_ID=$(echo $RESP | sed "s#{\"url\":\"$DEFECTDOJO_HOST_URL/engagements/##" | sed 's#/.*##')
 echo "ENGAGEMENT_ID $ENGAGEMENT_ID"
@@ -71,14 +72,17 @@ if ! is_int $ENGAGEMENT_ID; then
 fi
 
 echo "created engagement (develop) with id $ENGAGEMENT_ID"
-fillata1 $API_KEY $ENGAGEMENT_ID "False"
-fillata2 $API_KEY $ENGAGEMENT_ID "False"
+#filldata $API_KEY $ENGAGEMENT_ID "False"  "/media/encrypted/git/defectdojo_api/examples/dependency/dependency-check-report.xml"
+filldata $API_KEY $ENGAGEMENT_ID "False" "/media/encrypted/git/defectdojo_api/examples/dependency/dependency-check-report-minus-AppleJavaExtensions.xml"
 
-RESP=$(curl --silent --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'X-CSRFToken: rQ1QIKFTf5Ub17v1Fsghy4Z3JjeXMoaxwZ3liAud0qSE4yNHmRxFXlIkmEoM4ZrH' --header "Authorization: Token $API_KEY" -X POST -d '{    "branch_tag": "master", "version": "v1.0", "engagement_type": "CI/CD", "product": "'$PRODUCT_HYPERLINK'", "name": "CI/CD dep check master", "target_start": "2018-06-01", "target_end": "2018-06-01", "active": "True", "pen_test": "False", "check_list": "False", "threat_model": "False", "status": "In Progress", "deduplication_on_engagement": "True"}' "$DEFECTDOJO_HOST_URL/engagements/")
+
+RESP=$(curl --silent --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'X-CSRFToken: rQ1QIKFTf5Ub17v1Fsghy4Z3JjeXMoaxwZ3liAud0qSE4yNHmRxFXlIkmEoM4ZrH' --header "Authorization: Token $API_KEY" -X POST -d '{    "branch_tag": "master", "version": "v1.0", "engagement_type": "CI/CD", "product": "'$PRODUCT_HYPERLINK'", "name": "CI/CD dep check master", "target_start": "2018-06-01", "target_end": "2018-06-01", "active": "True", "pen_test": "False", "check_list": "False", "threat_model": "False", "status": "In Progress", "deduplication_on_engagement": "True", "lead": "/api/v2/users/1/"}' "$DEFECTDOJO_HOST_URL/engagements/")
 
 ENGAGEMENT_ID=$(echo $RESP | sed "s#{\"url\":\"$DEFECTDOJO_HOST_URL/engagements/##" | sed 's#/.*##')
 echo "created engagement (master) with id $ENGAGEMENT_ID"
-fillata1 $API_KEY $ENGAGEMENT_ID "True"
-fillata2 $API_KEY $ENGAGEMENT_ID "True"
-fillata3 $API_KEY $ENGAGEMENT_ID "True"
+#filldata $API_KEY $ENGAGEMENT_ID "True" "/media/encrypted/git/defectdojo_api/examples/dependency/dependency-check-report.xml"
+filldata $API_KEY $ENGAGEMENT_ID "True" "/media/encrypted/git/defectdojo_api/examples/dependency/dependency-check-report-minus-AppleJavaExtensions.xml"
+filldata $API_KEY $ENGAGEMENT_ID "True" "/media/encrypted/git/defectdojo_api/examples/dependency/dependency-check-report-minus-AppleJavaExtensions-minus-spring-txt.xml"
+
+
 
